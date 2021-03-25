eac10b23f80bb61ebdfdfc3f8d290ea2f9664709
DungVu0302 <vuvansin1999@gmail.com>
2021-03-25 13:28:21 +0700

    Đăng nhập thành công, chuyển qua màn hình mới

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-function login(){
-    let username = document.getElementById('username');
-    let pass = document.getElementById('pwd');
+function login() {
+  let username = document.getElementById("username");
+  let pass = document.getElementById("pwd");
 
-    if(username.value == 'admin' && pass.value == 'admin'){
-        alert("Đăng nhập thành công!!");
-    } else {        
-        alert("Đăng nhập thất bại!!");
-    }
+  if (username.value == "admin" && pass.value == "admin") {
+    alert("Đăng nhập thành công!!");
+    window.location = "ThanhCong.html";
+  } else {
+    alert("Đăng nhập thất bại!!");
+  }
 }
